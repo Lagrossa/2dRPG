02aef9aaf2188689c26af24f2966a5df7266d3f3
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-19 04:32:41 -0500

    Updated Files.

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -7,7 +7,8 @@ const FRICTION = 500
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
-
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	print("Solus")
 
@@ -17,7 +18,11 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if(input_vector != Vector2.ZERO):
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) 
 	velocity = move_and_slide(velocity)
