a2012de55bfd43bf0d1f7f5d4d859420213cd919
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-20 23:22:43 -0500

    Updated Files.

--- Grass.gd
+++ Grass.gd
@@ -4,4 +4,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("attack"):
 		var GrassEffect = load("res://Assets/Effects/GrassEffect.tscn")
 		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
 		queue_free()
